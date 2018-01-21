bccb41f774cb8bdaa57346ae5d1010eeecf28299
HANATANI Takuma <mail@potato4d.me>
2018-01-21 22:28:06 +0900

    Auth guardの実装

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class DashboardController < ApplicationController
+  before_action :auth_guard
+
   def index
     @user = current_user
   end
@@ -6,5 +8,9 @@ class DashboardController < ApplicationController
   private
   def current_user
     User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def auth_guard
+    redirect_to '/auth/twitter' unless current_user
   end
 end
