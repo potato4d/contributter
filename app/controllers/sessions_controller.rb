cd27b6b6be2152937795b22167f4ae22e6586036
HANATANI Takuma <mail@potato4d.me>
2018-01-23 13:30:38 +0900

    Redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,10 @@ class SessionsController < ApplicationController
   def create
     user = User.create_auth_user(request.env['omniauth.auth'])
     session[:user_id] = user.id
-    redirect_to '/dashboard'
+    if user.github_id
+      redirect_to '/dashboard'
+    else
+      redirect_to '/dashboard/settings'
+    end
   end
 end
