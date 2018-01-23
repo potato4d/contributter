17c977cc26ff7d6586ba4854c744ccb19ddb3666
HANATANI Takuma <mail@potato4d.me>
2018-01-23 12:48:23 +0900

    Exception and count

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,11 +2,28 @@ class AdminController < ApplicationController
 
   before_action :admin_auth
 
+  # Daily batch
   def crawl_and_tweet
     users = User.all
 
+    total_count = users.length
+    success_count = 0
+    fail_count = 0
+    skip_count = 0
+
     users.each do |user|
+      unless user.github_id
+        skip_count += 1
+        next
+      end
+
       user_contribution = Contribution.crawl_and_save(user)
+
+      if user_contribution
+        success_count += 1
+      else
+        fail_count += 1
+      end
     end
 
     render json: {
