872d0687c859436798e2fb1c4409a97fff16d397
HANATANI Takuma <mail@potato4d.me>
2018-01-23 13:12:43 +0900

    Admin exception

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,50 +7,56 @@ class AdminController < ApplicationController
 
   # Daily batch
   def crawl_and_tweet
-    users = User.all
+    begin
+      users = User.all
 
-    total_count = users.length
-    success_count = 0
-    fail_count = 0
-    skip_count = 0
+      total_count = users.length
+      success_count = 0
+      fail_count = 0
+      skip_count = 0
 
-    users.each do |user|
-      unless user.github_id
-        skip_count += 1
-        next
+      users.each do |user|
+        unless user.github_id
+          skip_count += 1
+          next
+        end
+
+        user_contribution = Contribution.crawl_and_save(user)
+
+        if user_contribution
+          success_count += 1
+        else
+          fail_count += 1
+        end
       end
 
-      user_contribution = Contribution.crawl_and_save(user)
+      # Post slack
+      # TODO: Modulize
 
-      if user_contribution
-        success_count += 1
-      else
-        fail_count += 1
+      text = "<@" + ENV['OWNER_NAME'] + ">\n" +
+        "[Contributter] " + Date.today.strftime("%Y/%m/%d") + "のバッチ実行結果\n\n" +
+        "```\n" +
+        "SUCCESS: " + success_count.to_s + "\n" +
+        "SKIP:    " + skip_count.to_s + "\n" +
+        "FAIL:    " + fail_count.to_s + "\n" +
+        "TOTAL:   " + total_count.to_s + "" +
+        "```"
+
+      conn = Faraday.new(:url => ENV['SLACK_API_ROOT'])
+      conn.post do |req|
+        req.url ENV["SLACK_API_ENDPOINT"]
+        req.headers['Content-Type'] = 'application/json'
+        req.body = '{"text": "' + text + '", "channel": "' + ENV['SLACK_CHANNEL'] + '"}'
       end
+
+      render json: {
+        result: true
+      }
+    rescue
+      render json: {
+        result: false
+      }
     end
-
-    # Post slack
-    # TODO: Modulize
-
-    text = "<@" + ENV['OWNER_NAME'] + ">\n" +
-      "[Contributter] " + Date.today.strftime("%Y/%m/%d") + "のバッチ実行結果\n\n" +
-      "```\n" +
-      "SUCCESS: " + success_count.to_s + "\n" +
-      "SKIP:    " + skip_count.to_s + "\n" +
-      "FAIL:    " + fail_count.to_s + "\n" +
-      "TOTAL:   " + total_count.to_s + "" +
-      "```"
-
-    conn = Faraday.new(:url => ENV['SLACK_API_ROOT'])
-    conn.post do |req|
-      req.url ENV["SLACK_API_ENDPOINT"]
-      req.headers['Content-Type'] = 'application/json'
-      req.body = '{"text": "' + text + '", "channel": "' + ENV['SLACK_CHANNEL'] + '"}'
-    end
-
-    render json: {
-      result: true
-    }
   end
 
   private
