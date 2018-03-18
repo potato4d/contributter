1d90243020aa821b0133847dab2ac80c354e7d3f
HANATANI Takuma <mail@potato4d.me>
2018-03-19 07:43:09 +0900

    :sparkles: Slack reporting decoration (#15)

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,6 @@
 require 'faraday'
 require 'date'
+require 'json'
 
 class AdminController < ApplicationController
 
@@ -32,21 +33,44 @@ class AdminController < ApplicationController
 
       # Post slack
       # TODO: Modulize
-
-      text = "<@" + ENV['OWNER_NAME'] + ">\n" +
-        "[Contributter] " + Date.today.strftime("%Y/%m/%d") + "のバッチ実行結果\n\n" +
-        "```\n" +
-        "SUCCESS: " + success_count.to_s + "\n" +
-        "SKIP:    " + skip_count.to_s + "\n" +
-        "FAIL:    " + fail_count.to_s + "\n" +
-        "TOTAL:   " + total_count.to_s + "" +
-        "```"
+      body = {
+        text: "<@" + ENV['OWNER_NAME'] + ">\n" + "[Contributter] " + Date.today.strftime("%Y/%m/%d") + "のバッチ実行結果\n\n",
+        channel: ENV['SLACK_CHANNEL']
+        attachments: [
+          {
+            color: '#F3E2C8',
+            text: 'Summary',
+            fields: [
+              {
+                title: '成功数',
+                value: success_count.to_s,
+                short: true
+              },
+              {
+                title: '失敗数',
+                value: fail_count.to_s,
+                short: true
+              },
+              {
+                title: 'スキップ数',
+                value: skip_count.to_s,
+                short: true
+              },
+              {
+                title: '合計ユーザー数',
+                value: total_count.to_s,
+                short: true
+              }
+            ]
+          }
+        ]
+      }
 
       conn = Faraday.new(:url => ENV['SLACK_API_ROOT'])
       conn.post do |req|
         req.url ENV["SLACK_API_ENDPOINT"]
         req.headers['Content-Type'] = 'application/json'
-        req.body = '{"text": "' + text + '", "channel": "' + ENV['SLACK_CHANNEL'] + '"}'
+        req.body = body.to_json
       end
 
       render json: {
