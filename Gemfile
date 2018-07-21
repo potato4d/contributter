d4049bc499668fa3b3ef356142053b7d5545b98a
HANATANI Takuma <mail@potato4d.me>
2018-07-21 11:09:10 +0900

    Use webpacker instead of sprockets (#20)
    * Use webpacker instead of sprockets
    
    * Add webpacker container to docker-compose

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
+
+gem 'webpacker', '~> 3.5'
+
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3'
 gem 'mysql2', '~> 0.4.0'
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,27 @@
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
+
+// stylesheets
+import './stylesheets/application.scss'
+
+// images
+import './images/logo.svg'
+import './images/logo_large.svg'
+import './images/logotype.svg'
+
+const $ = (el) => document.querySelector(el)
+
+window.addEventListener('load', () => {
+  if ($('.navbar-burger')) {
+    $('.navbar-burger').addEventListener('click', () => {
+      $('.navbar-burger').classList.toggle('is-active')
+      $('#navbar').classList.toggle('is-active')
+    })
+  }
+})
