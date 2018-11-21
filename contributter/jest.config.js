b2ac9f1ff3d28f226c92701a19cdbf2a3fbc574f
HANATANI Takuma <mail@potato4d.me>
2018-11-21 13:59:09 +0900

    :bomb: Create v2 project

---
+++ contributter/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  moduleFileExtensions: ["js", "json", "vue", "ts"],
+  transform: {
+    "^.+\\.vue$": "vue-jest",
+    ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
+      "jest-transform-stub",
+    "^.+\\.ts$": "ts-jest"
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^~/(.*)$": "<rootDir>/src/$1"
+  },
+  snapshotSerializers: ["jest-serializer-vue"],
+  testMatch: [
+    "**/tests/unit/**/*.spec.(js|ts)|**/__tests__/*.(js|ts)"
+  ],
+  testURL: "http://localhost/"
+};
