b81e97f4d9bd460c2238acc48ea2f06162ae9079
Takuma HANATANI <mail@potato4d.me>
2020-01-08 19:19:45 +0900

    chore: Update project structure
    - Migrate to GitHub Actions
    - Use Yarn Workspace

--- web/next.config.js
+++ web/next.config.js
@@ -10,8 +10,6 @@ function webpackConfig(config) {
       FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
       FIREBASE_DATABASE_URL: JSON.stringify(process.env.FIREBASE_DATABASE_URL),
       FIREBASE_PROJECT_ID: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-      FIREBASE_STORAGE_BUCKET: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-      FIREBASE_MESSAGING_SENDER_ID: JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
       FIREBASE_APP_ID: JSON.stringify(process.env.FIREBASE_APP_ID),
     })
   ]
